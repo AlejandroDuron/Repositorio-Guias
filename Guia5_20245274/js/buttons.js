462abe950711f0547506e40c97d44a7ff12ba713
AlejandroDuron <tato.duron@gmail.com>
2025-10-26 19:39:33 -0600

    Guia 4 y Estilizar pagina de proyectos personales web

---
+++ Guia5_20245274/js/buttons.js
@@ -0,0 +1,18 @@
+function aviso(){
+    alert("Bienvenido al mundo JavaScript")
+}
+
+function confirmacion() {
+    // los valores que almacena la variable confirmacion pueden ser true o false
+    let confirmacion = confirm("¿Desea salir de la Sesión?");
+    // para imprimir una variable en una cadena podemos usar comilla inversas y luego se declara la variables
+    alert(`Valor seleccionado ${confirmacion}`);
+}
+
+function capturarDatos() {
+    let nombre = prompt("¿Cual es su nombre?")
+    // el campo sera 0 por defecto
+    let edad = prompt("¿Cual es su edad?, 0")
+
+    alert(`Su nombre es ${nombre} y su edad ${edad}`)
+}
